5a187376e203bef30dbce7c9ded7e91fe46aa855
Phong Pham <ducphong2713@gmail.com>
2023-05-23 12:36:42 +0700

    feat/ Chức năng sửa sản phẩm refs:AGN-12

--- src/app/page/admin/admin-product-edit/admin-product-edit.component.ts
+++ src/app/page/admin/admin-product-edit/admin-product-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IProduct } from 'src/app/interfaces/Product';
+import { ProductService } from 'src/app/services/product.service';
+
 
 @Component({
   selector: 'app-admin-product-edit',
@@ -6,5 +10,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin-product-edit.component.scss']
 })
 export class AdminProductEditComponent {
-
+  product: IProduct = {
+    name: "",
+    price: 0,
+    img: ""
+  }
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute) {
+    this.route.paramMap.subscribe(param => {
+      const id = Number(param.get('id'));
+      this.productService.getProductById(id).subscribe(product => {
+        this.product = product;
+      }, error => console.log(error.message))
+    })
+  }
+  onHandleUpdate() {
+    this.productService.updateProduct(this.product).subscribe(product => {
+      console.log(product)
+    })
+  }
 }
