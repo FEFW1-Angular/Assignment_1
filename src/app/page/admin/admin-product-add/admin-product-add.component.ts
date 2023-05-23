4434409e5a0d78ccf1cc531902f74f862621f14e
Phong Pham <ducphong2713@gmail.com>
2023-05-23 12:29:08 +0700

    feat/Chức năng thêm sản phẩm refs:AGN-11

--- src/app/page/admin/admin-product-add/admin-product-add.component.ts
+++ src/app/page/admin/admin-product-add/admin-product-add.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { IProduct } from 'src/app/interfaces/Product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-admin-product-add',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin-product-add.component.scss']
 })
 export class AdminProductAddComponent {
+  product: IProduct = {
+    name: "",
+    price: 0,
+    img: ""
+  }
+  constructor(private productService: ProductService) {
 
+  }
+
+  onHandleAdd() {
+    this.productService.addProduct(this.product).subscribe(product => {
+      console.log(product)
+    })
+  }
 }
--- src/app/page/admin/admin-product/admin-product.component.ts
+++ src/app/page/admin/admin-product/admin-product.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { IProduct } from 'src/app/interfaces/Product';
 
 @Component({
   selector: 'app-admin-product',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin-product.component.scss']
 })
 export class AdminProductComponent {
+  @Input() products: IProduct[] = [];
+  @Output() onRemove = new EventEmitter<number>();
 
+  removeItem(id: any) {
+    this.onRemove.emit(id)
+    // this.products = this.products.filter((item) => item._id !== id);
+  }
 }
