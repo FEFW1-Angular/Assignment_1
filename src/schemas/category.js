739f944f7e32e00d1a48b02e67b9633ea8610f51
Phong Pham <ducphong2713@gmail.com>
2023-05-31 20:33:51 +0700

    feat/khoi-tao-backend-nodejs -Models -Scheme -CRUD category refs:AGN-41

---
+++ src/schemas/category.js
@@ -0,0 +1,12 @@
+import Joi from "joi";
+
+const categorySchema = Joi.object(
+    {
+        name: Joi.string().required().min(4).messages({
+            "string.empty": "Tên không được để trống",
+            "any.required": 'Trường "Tên" là bắt buộc',
+        }),
+        img: Joi.string().required()
+    }
+)
+export default categorySchema;
