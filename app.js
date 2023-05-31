210bc4859086afdd0d90c2184805d4e86bf8dc58
ductai0201 <ductai0201@gmail.com>
2023-05-31 19:10:41 +0700

    connect mongodb

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 import express from "express";
+import mongoose from "mongoose";
+
 const app = express();
 
-app.listen(8080, () => {
-  console.log("chạy ngon");
-});
+mongoose
+  .connect("mongodb://127.0.0.1:27017/angular")
+  .then(() => console.log("Kết nối tới MongoDB đã được thiết lập thành công!"))
+  .catch(err => console.log("Lỗi kết nối tới MongoDB:", err));
+
 
 export const viteNodeApp = app;
