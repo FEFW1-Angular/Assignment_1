bf1dba4cf62749a1dfc78e9752c9887745a378ea
truongmn203 <truongmnph21870@fpt.edu.vn>
2023-06-02 12:37:12 +0700

    feat/sua-danh-muc-reopen refs:AGN-18

--- src/app/page/admin/admin-category-edit/admin-category-edit.component.ts
+++ src/app/page/admin/admin-category-edit/admin-category-edit.component.ts
@@ -1,32 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ICategory } from 'src/app/interfaces/Category';
 import { CategoryService } from 'src/app/services/category.service';
-import { ActivatedRoute } from '@angular/router';
-
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-admin-category-edit',
   templateUrl: './admin-category-edit.component.html',
   styleUrls: ['./admin-category-edit.component.scss']
 })
-export class AdminCategoryEditComponent {
-  category: ICategory = {
-    name: "",
-    img: ""
-  }
+export class AdminCategoryEditComponent  {
+  category!: ICategory;
+  categoryForm = this.formBuilder.group({
+    name: ['', [Validators.required, Validators.minLength(4)]],
+    img: ['']
+  });
+
   constructor(
+    private formBuilder: FormBuilder,
     private categoryService: CategoryService,
-    private route: ActivatedRoute) {
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
     this.route.paramMap.subscribe(param => {
       const id = Number(param.get('id'));
       this.categoryService.getCategoryById(id).subscribe(category => {
         this.category = category;
+        this.categoryForm.patchValue({
+          name: category.name,
+          img: category.img
+        });
       }, error => console.log(error.message))
     })
-  }
-  onHandleUpdate() {
-    this.categoryService.updateCategory(this.category).subscribe(category => {
-      console.log(category)
-    })
-  }
+   }
 
+  onHandleUpdate() {
+    if (this.categoryForm.valid) {
+      const updatedCategory: ICategory = {
+        id: this.category.id,
+        name: this.categoryForm.value.name || "",
+        img: this.categoryForm.value.img || ""
+      };
+
+      this.categoryService.updateCategory(updatedCategory).subscribe(category => {
+        alert("Thêm danh mục thành công")
+        this.router.navigate(['/admin/category']);
+      });
+    }
+  }
 }
