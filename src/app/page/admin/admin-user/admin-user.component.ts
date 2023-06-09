cb0f104988cba88a3d87f09fadf003504dc7d425
Phong Pham <ducphong2713@gmail.com>
2023-06-09 12:18:54 +0700

    feat/hien-thi-list-user refs:AGN-106

--- src/app/page/admin/admin-user/admin-user.component.ts
+++ src/app/page/admin/admin-user/admin-user.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { IUser } from 'src/app/interfaces/User';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-admin-user',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin-user.component.scss']
 })
 export class AdminUserComponent {
+  users: IUser[] = [];
 
+  constructor(private userService: UserService) {
+    this.userService.getUsers().subscribe(data => {
+      this.users = data
+    }, error => {
+      console.log(error.message)
+    })
+  }
 }
