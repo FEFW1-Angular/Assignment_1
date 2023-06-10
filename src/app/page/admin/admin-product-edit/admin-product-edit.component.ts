dc5f79f7c4569bcfa1d82900793f0cc2572d84b1
Ha Van Anh <anhhvph21586@fpt.edu.vn>
2023-06-10 10:40:53 +0700

    feat: fix-update-product refs:AGN-15

--- src/app/page/admin/admin-product-edit/admin-product-edit.component.ts
+++ src/app/page/admin/admin-product-edit/admin-product-edit.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ICategory } from 'src/app/interfaces/Category';
 import { IProduct } from 'src/app/interfaces/Product';
+import { CategoryService } from 'src/app/services/category.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -10,35 +12,52 @@ import { ProductService } from 'src/app/services/product.service';
   styleUrls: ['./admin-product-edit.component.scss'],
 })
 export class AdminProductEditComponent {
+  categorys: ICategory[] = [];
+  
   product!: IProduct;
-  productForm = this.formBuilder.group({
-    name: ['', [Validators.required, Validators.minLength(4)]],
-    price: [0],
-    img: [''],
-  });
   constructor(
     private productService: ProductService,
     private route: ActivatedRoute,
     private formBuilder: FormBuilder,
-    private router: Router
+    private router: Router,
+    private categoryService: CategoryService
   ) {
     this.route.paramMap.subscribe((param) => {
       const id = this.route.snapshot.params['id'];
       this.productService.getProductById(id).subscribe(
         (product) => {
           this.product = product;
+          console.log(product)
           // set giá trị từ API vào input form
           this.productForm.patchValue({
             name: this.product.name,
             price: this.product.price,
             img: this.product.img,
+            category: (this.product.categoryId as any)?._id,
           });
         },
         (error) => console.log(error.message)
       );
+      this.categoryService.getCategorys().subscribe(
+        (data) => {
+          this.categorys = data;
+        },
+        (error) => {
+          console.log(error.message);
+        }
+      );
     });
-  }
+}
+  
+  productForm = this.formBuilder.group({
+    name: ['', [Validators.required, Validators.minLength(4)]],
+    price: [0],
+    img: [''],
+    category: [''],
+  });
+  
   onHandleUpdate() {
+    
     // kiểm tra nếu form hợp lệ
     if (this.productForm.valid) {
       const newProduct: IProduct = {
@@ -46,12 +65,13 @@ export class AdminProductEditComponent {
         name: this.productForm.value.name || '',
         img: this.productForm.value.img || '',
         price: this.productForm.value.price || 0,
+        categoryId: this.productForm.value.category || '',
       };
-      console.log(newProduct);
+      // console.log(newProduct._id);
 
       this.productService.updateProduct(newProduct).subscribe((product) => {
         alert('Cập nhật sản phẩm thành công');
-        this.router.navigate(['/admin/products']);
+        this.router.navigate(['/admin/product']);
       });
     }
   }
