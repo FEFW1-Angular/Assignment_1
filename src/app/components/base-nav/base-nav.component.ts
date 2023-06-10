b7969a877e1ab3cd4456e07fb881602a19d469a5
ductai0201 <ductai0201@gmail.com>
2023-06-10 07:01:24 +0700

    feat: login logout refs: AGN-102

--- src/app/components/base-nav/base-nav.component.ts
+++ src/app/components/base-nav/base-nav.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { ICategory } from 'src/app/interfaces/Category';
 import { CategoryService } from 'src/app/services/category.service';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-base-nav',
   templateUrl: './base-nav.component.html',
@@ -10,11 +10,20 @@ import { CategoryService } from 'src/app/services/category.service';
 export class BaseNavComponent {
   categorys: ICategory[] = [];
 
-  constructor(private categoryService: CategoryService) {
+  constructor(private categoryService: CategoryService, private router: Router) {
     this.categoryService.getCategorys().subscribe(data => {
       this.categorys = data
     }, error => {
       console.log(error.message)
     })
   }
+  isLoggedIn() {
+    return !!localStorage.getItem('currentUser');
+  }
+
+  logout() {
+    localStorage.removeItem('currentUser');
+    this.router.navigate(['/signin']);
+  }
+
 }
