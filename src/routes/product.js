ccb0d710a758c2db896e934e1fcd975e38c0167a
ductai0201 <ductai0201@gmail.com>
2023-06-13 02:54:36 +0700

    middleware

--- src/routes/product.js
+++ src/routes/product.js
@@ -7,14 +7,16 @@ import {
     removeProduct,
     searchProduct,
 } from "../controllers/product";
+import { checkPermission } from "../middlewares/checkPermission";
 
 const router = express.Router();
 
 router.get("/products", getAllProduct);
 router.get("/products/:id", getOneProduct);
-router.post("/products", createProduct);
+router.post("/products",checkPermission, createProduct);
+router.delete("/products/:id",checkPermission, removeProduct);
+router.patch("/products/:id",checkPermission, updateProduct);
 router.post("/products/search", searchProduct);
-router.delete("/products/:id", removeProduct);
-router.patch("/products/:id", updateProduct);
+
 
 export default router;
